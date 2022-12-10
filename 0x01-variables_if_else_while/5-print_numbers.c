80492009cfa9b6f9f53cba4057f05230e735f780
musowe <envoymalumbemusowe@gmail.com>
2022-12-09 16:43:55 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all single digit number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+
+		printf("%i", digit);
+		++digit;
+	}
+	printf("\n");
+
+	return (0);
+
+}

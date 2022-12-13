474b1b65bf46e31652cd6d5917521f02e7e6b176
musowe <envoymalumbemusowe@gmail.com>
2022-12-13 14:57:00 -0800

    correction task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,11 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43 + 0);
 		return (1);
-	} else if (n == 0)
-	{
-		_putchar('-');
 	}
+	else if (n == 0)
+	{
+		_putchar(0 + 48);
+	return (0);
+	}
+	else
+	{
+		_putchar(45 + 0);
 		return (-1);
+	}
 }

b29d96bc44eb386e9d870ab59aa656c7b8c6b1ff
musowe <envoymalumbemusowe@gmail.com>
2022-12-14 18:47:40 -0800

    task 9 corrections

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,31 +5,28 @@
  */
 void times_table(void)
 {
-	int i, j, k;
+	int num, mult, prod;
 
-	for (i = 0; i < 10; i++)
+	for (num = 0; num <= 9; ++num)
 	{
-		for (j = 0; j < 10; j++)
+		_putchar(48);
+		for (mult = 1; mult <= 9; ++mult)
 		{
-			k = j * i;
-			if (j == 0)
-			{
-				_putchar(k + '0');
-			}
+			_putchar(',');
+			_putchar(' ');
 
-			if (k < 10 && j != 0)
-			{
-				_putchar(',');
+			prod = num * mult;
+
+			/*
+			 * put space if product is a single number
+			 * place the first digit if its two numbers
+			*/
+			if (prod <= 9)
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			} else if (k >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
+			else
+				_putchar((prod / 10) + 48); /*get the first digit*/
+
+			_putchar((prod % 10) + 48); /*get the second digit*/
 		}
 		_putchar('\n');
 	}

8b2f9657cd0f9138f02461e1b270122cdaf12856
musowe <envoymalumbemusowe@gmail.com>
2022-12-23 10:35:39 -0800

    print number correction

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,18 +8,16 @@
 
 void print_number(int n)
 {
-	unsigned int n1;
-
-	n1 = n;
+	unsigned int num = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n1 = -n;
+		num = -num;
 	}
 
-	if (n1 / 10 != 0)
-		print_number(n1 / 10);
+	if (num / 10 > 0)
+		print_number(num / 10);
 
-	_putchar((n1 % 10) + '0');
+	_putchar((num % 10) + '0');
 }

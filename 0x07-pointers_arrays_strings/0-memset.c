394791ffa7f337b3f71eab5f0b1089d390b0ba71
musowe <envoymalumbemusowe@gmail.com>
2023-01-02 15:44:50 -0800

    correction for task 0 memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,15 @@
-**/
+/**
  * _memset - a function that fills
- *          memory with a constant byte
+ *         memory with a constant byte
  *
  * @s: input pointer to char type
  *    represents the pointer to the
  *    block of memory to fill
  * @b: input variable of char type
- *    represents the character to
- *    fill s
+ *     represents the character to
+ *     fill s
  * @n: unsigned int variable
- *    the number of bytes to be filled
+ *     the number of bytes to be filled
  *
  * Return: A pointer to the filled memory
  *         area @s

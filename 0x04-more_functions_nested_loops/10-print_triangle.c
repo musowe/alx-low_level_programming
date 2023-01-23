836ad667fea9b984cf1b94a87f71377fea6948a5
musowe <envoymalumbemusowe@gmail.com>
2023-01-23 08:43:27 -0800

    task 10 print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,7 +4,6 @@
  * print_triangle - prints a triangle, followed by a new line
  * @size: size of the triangle
  */
-
 void print_triangle(int size)
 {
 	if (size <= 0)
@@ -19,6 +18,11 @@ void print_triangle(int size)
 			for (j = i; j < size; j++)
 			{
 				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
 			}
 
 			_putchar('\n');

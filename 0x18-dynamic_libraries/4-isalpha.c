468814c95e1432f6f92e16837db655797f84bde5
musowe <envoymalumbemusowe@gmail.com>
2023-06-23 11:47:42 -0700

    task 18 dynamic libraries tasks

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * _isalpha - checks if character is a letter
+ *            both lowercase or uppercase
+ *
+ * @c: takes input from other functions.
+ *
+ * Return: 1 is c if true else 0
+ */
+
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
